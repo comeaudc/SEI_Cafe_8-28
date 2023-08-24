409606cc22695fbfeb9a72adc002af0d4a3be646
tmarcelojr <tmarcelojr@gmail.com>
2023-08-24 11:23:59 -0500

    added backend and frontend

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+// pages
+import AuthPage from "./pages/AuthPage/AuthPage";
+import NewOrderPage from "./pages/NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "./pages/OrderHistoryPage/OrderHistoryPage";
+// components
+import NavBar from "./components/NavBar/NavBar";
+// css
+import "./App.css";
+
+function App() {
+  // array destructuring
+  const [user, setUser] = useState({});
+
+  return (
+    <main className="App">
+      {user ? 
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+       : 
+        <AuthPage />
+      }
+    </main>
+  );
+}
+
+export default App;
