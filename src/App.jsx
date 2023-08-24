dac45e7d4320bbea5e2d83d016a864f97ebe410a
tmarcelojr <tmarcelojr@gmail.com>
2023-08-24 14:50:59 -0500

    rendering completed sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import "./App.css";
 
 function App() {
   // array destructuring
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   return (
     <main className="App">
